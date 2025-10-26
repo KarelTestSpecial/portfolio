9b6d3430c69f4223129235bde6baf2658b2c78e7
KarelTestSpecial <karel.test.special@gmail.com>
2025-10-26 19:47:17 +0100

    Remove href attribute from navbar brand link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
     <header>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
-          <a className="navbar-brand" href="/">My Portfolio</a>
+          <a className="navbar-brand">My Portfolio</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
